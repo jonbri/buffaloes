61f82f228867862e72490cd1eb1a0b1ea0c927cf
Jonathan Brink <jonathandavidbrink@gmail.com>
2025-07-16 09:47:28 -0400

    feat: --text

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 
-var i = 0,
-  num = process.argv[2],
-  buffaloString = "";
+const { program } = require("commander");
 
-for (; i < num; i++) {
-  buffaloString += "buffalo";
-  if (i < (num - 1)) {
-    buffaloString += " ";
-  }
-}
+program
+  .option("-t, --text <char>", 'Default: "buffalo"')
+  .argument("[number]", "Number of times to repeat the text")
+  .parse();
+const [numArg] = program.args;
+const { text = "buffalo" } = program.opts();
+
+const num = parseInt(numArg, 10);
+const buffaloString = Array.from({ length: num }, (_, index) => text).reduce(
+  (acc, curr, index) => acc + curr + (index < num - 1 ? " " : ""),
+  "",
+);
 
 if (buffaloString !== "") {
   console.log(buffaloString);
--- release.config.js
+++ release.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   branches: ["main"],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/npm",
-  ],
+  plugins: ["@semantic-release/commit-analyzer", "@semantic-release/npm"],
 };
